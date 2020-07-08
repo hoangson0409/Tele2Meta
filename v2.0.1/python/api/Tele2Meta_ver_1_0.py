9d6c7e320e79735f69ddeb8600c02dd08f272e62
hoangson0409 <hoangson0409@gmail.com>
2020-07-08 05:29:12 +0000

    add tele2meta ver 1 for gcp

--- v2.0.1/python/api/Tele2Meta_ver_1_0.py
+++ v2.0.1/python/api/Tele2Meta_ver_1_0.py
@@ -1,6 +1,7 @@
 import os
 
-path2 = 'C:\\Users\\Admin\\Downloads\\dwx-zeromq-connector-master\\v2.0.1\\python\\api'
+#path2 = 'C:\\Users\\Admin\\Downloads\\dwx-zeromq-connector-master\\v2.0.1\\python\\api'
+path2 = 'C:\\Users\\hoangson0409\\Downloads\\Tele2Meta\\v2.0.1\\python\\api'
 
 os.chdir(path2)
 
@@ -25,7 +26,8 @@ from Tele2Meta_support_function import deEmojify, orderTypeEncode, priceToPoints
 
 
 # Reading Configs
-path3 = 'C:\\Users\\Admin\\Downloads\\dwx-zeromq-connector-master\\telegram-analysis-master'
+#path3 = 'C:\\Users\\Admin\\Downloads\\dwx-zeromq-connector-master\\telegram-analysis-master'
+path3 = 'C:\\Users\\hoangson0409\\Downloads\\Tele2Meta\\telegram-analysis-master'
 
 os.chdir(path3)
 config = configparser.ConfigParser()
